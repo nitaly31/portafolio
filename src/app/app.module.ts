3bfdfbcd600f83a163ff180761e05b9566d8ef13
Geraldine Nitaly Meneses <geraldine.nitaly@gmail.com>
2023-03-15 11:06:19 -0500

    feat(portafolio): creacion de componente portafolo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { PortafolioComponent } from './components/portafolio/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent
+    HeroComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule,
